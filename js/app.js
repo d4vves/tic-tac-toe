5e0136ea46472e821aab10ce589aae90c4962517
d4vves <d4vves@gmail.com>
2020-05-31 11:56:02 -0500

    combined X and O evaluate functions into evaluateCombinations(), added changeTurn(), improved gameDraw()

--- js/app.js
+++ js/app.js
@@ -44,32 +44,47 @@ function cellClick(e) {
             cellStates[choice] = true;
             xPlayer.push(choice);
             e.target.classList.add("xSelect");
-            playerTurn++;
-            gameMessage.innerText = "oh my! it's O's turn."
-            evaluateXCombinations();
+            changeTurn();
+            evaluateCombinations();
         } else {
             cellStates[choice] = true;
             oPlayer.push(choice);
             e.target.classList.add("oSelect");
-            playerTurn--;
-            gameMessage.innerText = "x gonna give it to ya."
-            evaluateOCombinations();
+            changeTurn();
+            evaluateCombinations();
         }
     } 
 }
 
-function evaluateXCombinations() {
-    for (let i = 0; i < winStates.length; i++) {
-        let winningCombo = winStates[i];
-        let match = 0;
-        for (let j = 0; j < winningCombo.length; j++) {
-            if (xPlayer.includes(winningCombo[j])) {
-                match++;
-            } if (match === 3) {
-                gameMessage.innerText = "player X winz!"
-                gameBoard.removeEventListener("click", cellClick);
-                xWins++;
-                xScore.innerText = xWins;
+function evaluateCombinations() {
+    if (playerTurn) {
+        for (let i = 0; i < winStates.length; i++) {
+            let winningCombo = winStates[i];
+            let match = 0;
+            for (let j = 0; j < winningCombo.length; j++) {
+                if (xPlayer.includes(winningCombo[j])) {
+                    match++;
+                } if (match === 3) {
+                    gameMessage.innerText = "player X winz!"
+                    gameBoard.removeEventListener("click", cellClick);
+                    xWins++;
+                    xScore.innerText = xWins;
+                }
+            }
+        }
+    } else {
+        for (let i = 0; i < winStates.length; i++) {
+            let winningCombo = winStates[i];
+            let match = 0;
+            for (let j = 0; j < winningCombo.length; j++) {
+                if (oPlayer.includes(winningCombo[j])) {
+                    match++;
+                } if (match === 3) {
+                    gameMessage.innerText = "player O winz!"
+                    gameBoard.removeEventListener("click", cellClick);
+                    oWins++;
+                    oScore.innerText = oWins;
+                }
             }
         }
     }
@@ -78,25 +93,19 @@ function evaluateXCombinations() {
     }
 }
 
-function evaluateOCombinations() {
-    for (let i = 0; i < winStates.length; i++) {
-        let winningCombo = winStates[i];
-        let match = 0;
-        for (let j = 0; j < winningCombo.length; j++) {
-            if (oPlayer.includes(winningCombo[j])) {
-                match++;
-            } if (match === 3) {
-                gameMessage.innerText = "player O winz!"
-                gameBoard.removeEventListener("click", cellClick);
-                oWins++;
-                oScore.innerText = oWins;
-            }
-        }
+function changeTurn() {
+    if (!playerTurn) {
+        playerTurn++;
+        gameMessage.innerText = "oh my! it's O's turn."
+    } else {
+        playerTurn--;
+        gameMessage.innerText = "x gonna give it to ya."
     }
 }
 
 function gameDraw() {
     gameMessage.innerText = "tis a draw!";
+    gameBoard.removeEventListener("click", cellClick);
 }
 
 function resetGame() {
